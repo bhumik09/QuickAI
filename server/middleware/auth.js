1f73ec0ec8cd2b13d5e48f2dd66dc8b56df6bceb
Bhumika Goyal <162708059+bhumik09@users.noreply.github.com>
2026-06-21 18:15:41 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ import { clerkClient } from "@clerk/express";
 
 export const auth = async (req, res, next) => {
   try {
-    const { userId, has } = await req.auth();
+    const { userId, has } = await req.auth()
     const hashPremiumPlan = await has({ plan: 'premium' });
     const user = await clerkClient.users.getUser(userId);
     if (!hashPremiumPlan && user.privateMetadata.free_usage) {
@@ -12,7 +12,7 @@ export const auth = async (req, res, next) => {
       await clerkClient.users.updateUserMetadata(userId, {
         privateMetadata: { free_usage: 0 }
       });
-      req.free_usage = 0;
+      req.fre_usage = 0;
     }
     req.plan = hashPremiumPlan ? 'premium' : 'free';
     next();
